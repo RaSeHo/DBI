7e0e3be62f685905cdb60562583ca44431b5da9b
RaSeHo <53177616+RaSeHo@users.noreply.github.com>
2023-02-14 04:32:34 +0200

    Create slots.gd

---
+++ slots.gd
@@ -0,0 +1,28 @@
+#Ugly solution, but faster than move entire children.
+@tool
+
+extends Node2D
+
+@export var sl_oder = [] : set = slots_oder
+
+func set_rest():
+	if get_children().size()!=0:
+		for i in get_children().size():
+			get_child(i).z_index=i
+
+func to_rest():
+	if get_children().size()!=0:
+		for i in get_child_count():
+			get_child(i).z_index=i
+
+func slots_oder(arr=[]):
+	if not (sl_oder==arr):
+		sl_oder=arr.duplicate();
+		to_rest()
+		if get_children().size()!=0 && arr.size()!=0 && arr.size()%2==0:
+			var buf = get_children().duplicate();
+			for i in range(0,arr.size(),2):
+				buf.remove_at(arr[i]);
+				buf.insert(arr[i]+arr[i+1],get_child(arr[i]));
+			for i in buf.size():
+				buf[i].z_index=i;
